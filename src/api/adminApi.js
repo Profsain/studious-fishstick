0b41e8103015443e8398756bb4c8064d386aa4f2
profsain <husseinimudiking@gmail.com>
2024-09-20 02:30:27 +0100

    finish login and forgot pass

--- src/api/adminApi.js
+++ src/api/adminApi.js
@@ -1,15 +1,15 @@
 // api/adminApi.js
 import axios from 'axios';
 
-const API_URL = 'https://splinx-server.onrender.com';
+const API_URL = process.env.REACT_APP_API_URL;
 
 const loginAdmin = async (emailAddress, password) => {
     try {
-        const response = await axios.post(`${API_URL}/admin-login`, {
+        const response = await axios.post(`${API_URL}/admin/admin-login`, {
             emailAddress,
             password,
         });
-
+        
         return response.data; // This will contain the token and admin details
     } catch (error) {
         console.error('Error logging in:', error);
