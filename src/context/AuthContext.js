f85fae1bc413bb480dbbcddf428c0bc6caf455ee
missionctrlbiz <teewhy_007@yahoo.com>
2024-09-29 15:16:03 +0100

    feat: Enhance Advert Management Feature

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -9,38 +9,47 @@ export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
   const [userId, setUserId] = useState(null);
 
-
-  // useEffect to retrieve token from local storage on app load
+  // useEffect to retrieve token and userId from local storage on app load
   useEffect(() => {
     const storedToken = localStorage.getItem('token');
+    const storedUserId = localStorage.getItem('userId');
+
     if (storedToken) {
       setToken(storedToken);
     }
+    if (storedUserId) {
+      setUserId(storedUserId); 
+    }
   }, []);
 
-  // useEffect to store token in local storage whenever it changes
+  // useEffect to store token and userId in local storage whenever they change
   useEffect(() => {
     if (token) {
       localStorage.setItem('token', token);
     } else {
       localStorage.removeItem('token');
     }
-  }, [token]);
 
-  // Assuming you get userId during authentication
+    if (userId) {
+      localStorage.setItem('userId', userId);
+    } else {
+      localStorage.removeItem('userId');
+    }
+  }, [token, userId]); 
+
   const handleLogin = (userData) => {
     setUser(userData);
     setToken(userData.token);
-    setUserId(userData.userId); // Set userId when user logs in
+    setUserId(userData.userId); 
   };
 
-  // Add handleLogin to the context value
   return (
     <AuthContext.Provider value={{ user, setUser, token, setToken, userId, handleLogin }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
 // Custom Hook for using AuthContext
 export const useAuth = () => useContext(AuthContext);
 export default AuthContext;
